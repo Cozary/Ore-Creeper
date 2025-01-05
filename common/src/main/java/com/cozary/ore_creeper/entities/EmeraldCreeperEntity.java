acdd189165b9cc50ea34301fd49533f1def092b9
Cozary <ayrecio99@gmail.com>
2025-01-05 21:17:36 +0100

    tags added to canOreCreeperSpawn

--- common/src/main/java/com/cozary/ore_creeper/entities/EmeraldCreeperEntity.java
+++ common/src/main/java/com/cozary/ore_creeper/entities/EmeraldCreeperEntity.java
@@ -1,5 +1,6 @@
 package com.cozary.ore_creeper.entities;
 
+import com.cozary.ore_creeper.init.ModTags;
 import com.cozary.ore_creeper.init.ParticleList;
 import com.cozary.ore_creeper.util.ConfigurationHandler;
 import com.cozary.ore_creeper.util.ExplosionTypes;
@@ -22,7 +23,7 @@ public class EmeraldCreeperEntity extends AbstractOreCreeperEntity {
 
 
     public static boolean canOreCreeperSpawn(EntityType<? extends AbstractOreCreeperEntity> creeper, ServerLevelAccessor world, MobSpawnType reason, BlockPos pos, RandomSource random) {
-        return pos.getY() < ConfigurationHandler.GENERAL.emeraldCreeperMaxSpawnYLevel.get() && (world.getBlockState(pos.below()).is(Blocks.STONE) || world.getBlockState(pos.below()).is(Blocks.DEEPSLATE)) && isDarkEnoughToSpawn(world, pos, random) && checkMobSpawnRules(creeper, world, reason, pos, random);
+        return pos.getY() < ConfigurationHandler.GENERAL.emeraldCreeperMaxSpawnYLevel.get() && world.getBlockState(pos.below()).is(ModTags.SPAWNABLE_BLOCKS) && isDarkEnoughToSpawn(world, pos, random) && checkMobSpawnRules(creeper, world, reason, pos, random);
     }
 
 
