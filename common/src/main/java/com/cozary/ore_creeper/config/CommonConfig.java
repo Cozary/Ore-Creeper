3fa59d782278de49c8bf04742c09b6c0c0dce283
Cozary <ayrecio99@gmail.com>
2025-05-16 20:38:16 +0200

    oreCreepersExplodeLikeNormalCreepers set to false

--- common/src/main/java/com/cozary/ore_creeper/config/CommonConfig.java
+++ common/src/main/java/com/cozary/ore_creeper/config/CommonConfig.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 public class CommonConfig {
 
     @SerializedName("oreCreepersExplodeLikeNormalCreepers")
-    private boolean oreCreepersExplodeLikeNormalCreepers = true;
+    private boolean oreCreepersExplodeLikeNormalCreepers = false;
 
     public boolean oreCreepersExplodeLikeNormalCreepers() {
         return oreCreepersExplodeLikeNormalCreepers;
